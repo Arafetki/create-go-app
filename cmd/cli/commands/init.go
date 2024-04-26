2e743332fbb1b0fe4c6d6b8674ff64fb4ac2048d
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-04-26 17:35:29 +0100

    thinking about project design

--- cmd/cli/commands/init.go
+++ cmd/cli/commands/init.go
@@ -2,11 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/arafetki/create-go-app/internal/licence"
 	"github.com/spf13/cobra"
 )
 
@@ -15,50 +11,9 @@ var initCmd = &cobra.Command{
 	Short: "Initialize a new Go project",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		var (
-			projectName  string
-			userFullName string
-		)
+		// @Todo
 
-		if len(args) == 0 {
-			projectName = StringPrompt("Enter Project Name")
-
-		} else {
-			projectName = args[0]
-		}
-		userFullName = StringPrompt("Enter Your FullName")
-
-		currentDir, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Error getting current directory:", err)
-			return
-		}
-
-		projectPath := filepath.Join(currentDir, projectName)
-
-		if _, err := os.Stat(projectPath); os.IsNotExist(err) {
-
-			err = os.Mkdir(projectName, 0755)
-			if err != nil {
-				fmt.Println("Error creating project directory:", err.Error())
-				return
-			}
-		} else {
-			if !YesNoPrompt("Do you want to override existing files?", false) {
-				fmt.Println("Aborting initialization.")
-				return
-			} else {
-				fmt.Println(("Overriding existing files..."))
-			}
-		}
-
-		err = licence.GenerateMIT(projectPath, userFullName, time.Now().Year())
-		if err != nil {
-			fmt.Println("Error generating LICENSE file:", err.Error())
-			return
-		}
-
-		fmt.Println("Project initialized successfully.")
+		fmt.Println("Do something!")
 
 	},
 }
